4fba7aae9f857c8972f9c4d7e867a5831e3c58ea
Eugene <eugenemrlens@gmail.com>
2022-01-22 11:57:50 +0300

    add favorites

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,20 +1,39 @@
+import { useContext } from 'react';
+import FavoritesContext from '../../store/favorites-context';
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css'
 
 function MeetupItem(props) {
+  const favoritesContext = useContext(FavoritesContext)
+
+  const itemIsFavorite = favoritesContext.itemIsFavorite(props.id)
+  function toggleFavoriteStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesContext.removeFavorite(props.id)
+    } else {
+      favoritesContext.addFavorite({
+        id: props.id,
+        title: props.title,
+        image: props.image,
+        description: props.description,
+        address: props.address,
+      })
+    }
+  }
+
   return <li className={classes.item}>
     <Card>
-    <div className={classes.image}>
-      <img src={props.image} alt={props.title} />
-    </div>
-    <div className={classes.content}>
-      <h3>{props.title}</h3>
-      <address>{props.address}</address>
-      <p>{props.description}</p>
-    </div>
-    <div className={classes.actions}>
-      <button>To Favorites</button>
-    </div>
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={toggleFavoriteStatusHandler}>{itemIsFavorite ? 'Remove from Favorites' : 'To Favorites'}</button>
+      </div>
     </Card>
   </li>
 }
