5f81232dc97c3f137a33bac4539cb364fb45fa98
Eugene <eugenemrlens@gmail.com>
2022-01-22 10:01:07 +0300

    fetching data from firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,13 +1,24 @@
+import { useNavigate } from 'react-router-dom'
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 function NewMeetupPage() {
 
+  const navigate = useNavigate()
+
   function addMeetUpHandler(meetUpData) {
-    
+    fetch('https://students-corner-7520a.firebaseio.com/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetUpData),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(() => { 
+      navigate('/')
+    })
   }
 
   return <section><h1>Add New Meetup</h1>
-    <NewMeetupForm onAddMeetUp={addMeetUpHandler}/>
+    <NewMeetupForm onAddMeetUp={addMeetUpHandler} />
   </section>
 }
 
